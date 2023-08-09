f33bdda862d5a4979fc465709bf1147e576beb8a
Ion Carai <dcarai@mail.ru>
2023-08-09 13:31:06 +0300

    task three

---
+++ script.js
@@ -0,0 +1,14 @@
+let age = 18;
+
+if (age < 18) {
+    console.log  ("Ești prea tânăr pentru a juca acest joc");
+}
+
+if (age >= 18) {
+    console.log ("Ești potrivit pentru a juca acest joc");
+}
+console.log(age)
+
+
+
+
