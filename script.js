af72d1de45e0807d05be1041ee375603e4d2b9cf
Ion Carai <dcarai@mail.ru>
2023-08-15 22:30:34 +0300

    I finished the first homework from the java script module

--- script.js
+++ script.js
@@ -57,3 +57,69 @@ const limitaSuperioara = 32;
 const rezultat = verificaLimita(numar, limitaInferioara, limitaSuperioara);
 console.log(rezultat);
 
+//Sarcina 5
+function convertCurrency(value, fromCurrency, toCurrency) {
+    const exchangeRates = {
+        "USD": 18.02,  
+        "MDL": 21.12,  
+        "RON": 4.01    
+    };
+
+    let baseAmount;
+    if (fromCurrency === "EUR") {
+        baseAmount = value;
+    } else {
+        baseAmount = value / exchangeRates[fromCurrency];
+    }
+
+    let results;
+    if (toCurrency === "EUR") {
+        results = baseAmount;
+    } else {
+        results = baseAmount * exchangeRates[toCurrency];
+    }
+
+    return results.toFixed(2);
+}
+
+const inputValue = 10; 
+const fromCurrency = "USD"; 
+const toCurrency = "MDL";   
+
+const results = convertCurrency(inputValue, fromCurrency, toCurrency);
+console.log(`${inputValue} ${fromCurrency} sunt echivalentul a ${results} ${toCurrency}`);
+
+
+
+//Sarcina 6
+function checkRectangleType(length, width) {
+    if (length === width) {
+        return "Dreptunghiul este un pătrat.";
+    } else {
+        return "Dreptunghiul nu este un pătrat.";
+    }
+}
+
+let length = 5;
+let width = 5;
+
+let result = checkRectangleType(length, width);
+console.log(result);
+
+//Sarcina 7
+function getDayName(dayNumber) {
+    let daysOfWeek = [
+        "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă", "Duminică"
+    ];
+
+    if (dayNumber >= 1 && dayNumber <= 7) {
+        return daysOfWeek[dayNumber - 1];
+    } else {
+        return "Numărul introdus nu corespunde unei zile de săptămână.";
+    }
+}
+
+let dayNumber = 3;
+
+let dayName = getDayName(dayNumber);
+console.log(dayName);
